e17792c29527044e7cca4ad49e7633c1d8a4c51e
Abdulbaki Dursun <eggsydev@outlook.com>
2020-05-15 02:08:08 +0300

    :fire: New option: twitter_card (and site_name)!

--- README.md
+++ README.md
@@ -1,30 +1,38 @@
 # Nuxt social meta
 
-Nuxt.js module generate meta-tags for social network - facebook and twitter.
+Nuxt.js module generate meta-tags for social networks - Facebook, Twitter and LinkedIn (and the rest uses OG tags, such as Discord etc.).
 
 ## Install
 
 ```sh
-npm i --save nuxt-social-meta
+npm i nuxt-social-meta --save
+
+# or, with yarn:
+
+yarn add nuxt-social-meta
 ```
 
 ## Usage
 
-Add module to nuxt.config.js
+Add module to your `nuxt.config.js`
 
 ```js
 module.exports = {
   modules: [
-    ...
-    ['nuxt-social-meta', {
-      url: 'Site url',
-      title: 'Title site',
-      description: 'Description site',
-      img: 'Link to image in static folder',
-      locale: 'ru_RU',
-      twitter: '@UserName',
-      themeColor: '#ThemeColor'
-    }]
-  ]
-}
+    ...[
+      "nuxt-social-meta",
+      {
+        url: "Site url",
+        title: "Title",
+        site_name: "Site name",
+        description: "Site description",
+        img: "Link to image in static folder",
+        locale: "en_US",
+        twitter: "@user",
+        twitter_card: "summary_large_image",
+        themeColor: "#theme-color",
+      },
+    ],
+  ],
+};
 ```
--- index.js
+++ index.js
@@ -1,35 +1,29 @@
-module.exports = function socialMeta (options) {
-  
-  // options.url
-  // options.title
-  // options.description
-  // options.img
-  // options.locale
-  // options.twitter
-  // options.themeColor
-
+module.exports = function socialMeta(options) {
   // All meta tags
-  var metaTags = [
+  const metaTags = [
     // Global
-    { name: 'author', content: options.url },
-    { name: 'publisher', content: options.url },
-    { name: 'apple-mobile-web-app-title', content: options.title },
-    { name: 'theme-color', content: options.themeColor },
-    // Fb
-    { name: 'og:title', content: options.title },
-    { name: 'og:description', content: options.description },
-    { name: 'og:type', content: 'website' },
-    { name: 'og:url', content: options.url },
-    { name: 'og:image', content: options.img },
-    { name: 'og:locale', content: options.locale },
+    { name: "author", content: options.url },
+    { name: "publisher", content: options.url },
+    { name: "apple-mobile-web-app-title", content: options.title },
+    { name: "theme-color", content: options.themeColor },
+
+    // Facebook & LinkedIn
+    { name: "og:title", content: options.title },
+    { name: "og:description", content: options.description },
+    { name: "og:type", content: "website" },
+    { name: "og:url", content: options.url },
+    { name: "og:image", content: options.img },
+    { name: "og:locale", content: options.locale },
+    { name: "og:site_name", content: options.site_name },
+
     // Twitter
-    { name: 'twitter:card', content: 'summary_large_image' },
-    { name: 'twitter:site', content: options.twitter },
-    { name: 'twitter:creator', content: options.twitter },
-    { name: 'twitter:title', content: options.title },
-    { name: 'twitter:description', content: options.description },
-    { name: 'twitter:image', content: options.img }
-  ]
+    { name: "twitter:card", content: options.twitter_card },
+    { name: "twitter:site", content: options.twitter },
+    { name: "twitter:creator", content: options.twitter },
+    { name: "twitter:title", content: options.title },
+    { name: "twitter:description", content: options.description },
+    { name: "twitter:image", content: options.img },
+  ];
 
   // Add meta tags to head
   metaTags.forEach((tag) => {
@@ -37,11 +31,10 @@ module.exports = function socialMeta (options) {
       this.options.head.meta.push({
         hid: tag.name,
         name: tag.name,
-        content: tag.content
-      })
+        content: tag.content,
+      });
     }
-  })
+  });
+};
 
-}
-
-module.exports.meta = require('./package.json')
+module.exports.meta = require("./package.json");
